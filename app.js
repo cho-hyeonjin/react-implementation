cef4a7bb4e843c9c3a751a0295e09abe7d1e7564
cho-hyeonjin <gusls2005@gmail.com>
2024-03-17 23:18:26 +0900

    feat: children의 값이 객체가 아닌 문자열인 경우 예외처리

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 // 2. HTML tag를 객체로 바꿔서 리턴해보자. --- (다루기 쉬운 포맷을 다시 다루기 까다로운 포맷으로)
 function createDOM(node) {
-  // document.creatElement API를 이용하되 태그명에 해당하는 문자열을 HTML을 표현한 객체에서 notation으로 끌어온다.
-  const element = document.createElement(node.tag); // === document.createElement('p')
+  // 5. 문자열이면 문자열 DOM을 만들어서 리턴하도록 예외처리
+  if (typeof node === "string") {
+    return document.createTextNode(node);
+  }
+
+  // 2-1. document.creatElement API를 이용하되 태그명에 해당하는 문자열을 HTML을 표현한 객체에서 notation으로 끌어온다.
+  const element = document.createElement(node.tag);
 
   // 4-2. 재귀 패턴으로 createDOM 함수를 호출하여 element의 자식요소 붙이기
-  // a라는 값을 b로 바꿀땐? --> map 메서드
-  // map 메서드로 재귀함수 패턴을 이용하여 자식요소를 생성해준다.
-  // (children의 객체들 또한 virtualDOM 객체와 형태는 동일하고,
-  // children이 몇 개나 중첩될지 모르기 때문에 재귀 패턴 이용이 적합!)
   node.children.map(createDOM).forEach(element.appendChild.bind(element)); // 컨텍스트가 깨지기 때문에 bind로 묶어서 고정해준 것!
 
+  // 2-2. HTML element 리턴
   return element;
 }
 
