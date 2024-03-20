de205428871d313e2587572ea5bbddfaca58fb0a
cho-hyeonjin <gusls2005@gmail.com>
2024-03-20 23:53:39 +0900

    feat: JSX 구현-react 사용부 DOM 방식에서 마크업 방식으로

--- src/app.js
+++ src/app.js
@@ -14,5 +14,19 @@ const virtualDOM = createElement(
     createElement("li", { style: "color: green" }, "아이템3")
   )
 );
-
 render(virtualDOM, document.querySelector("#root"));
+
+const virtualDOM2 = (
+  <p>
+    <h1>React Implementation</h1>
+    <ul>
+      <li style="color: red">아이템1</li>
+      <li style="color: blue">아이템2</li>
+      <li style="color: green">아이템3</li>
+    </ul>
+  </p>
+);
+
+console.log(virtualDOM2);
+
+render(virtualDOM2, document.querySelector("#root"));
