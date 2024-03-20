17a9b043264d8320476865a8515c8704668fbd9c
cho-hyeonjin <gusls2005@gmail.com>
2024-03-20 22:21:49 +0900

    feat: react의 createDOM 함수를 이용하여 VirtualDOM 생성하기

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import { createDOM, render } from "./react";
 import { createElement } from "./react";
 
+// 하드 코딩으로 만드는 VirtualDOM
 const virtualDOM = {
   tag: "p",
   props: {},
@@ -34,9 +35,21 @@ const virtualDOM = {
   ],
 };
 
+render(virtualDOM, document.querySelector("#root"));
+
+// createElement 함수로 만드는 VirtualDOM
 const virtualDOM2 = createElement(
   "p",
   {},
-  createElement("h1", {}, "React Implementation")
+  createElement("h1", {}, "React Implementation"),
+  createElement(
+    "ul",
+    {},
+    createElement("li", { style: "color: red" }, "아이템1"),
+    createElement("li", { style: "color: blue" }, "아이템2"),
+    createElement("li", { style: "color: green" }, "아이템3")
+  )
 );
-render(virtualDOM, document.querySelector("#root"));
+
+console.log(virtualDOM2);
+render(virtualDOM2, document.querySelector("#root"));
