2483cacf596cd76e6addeb047b87727f83856fe2
cho-hyeonjin <gusls2005@gmail.com>
2024-03-21 00:01:43 +0900

    refactor: react 사용부 DOM 방식에서 마크업 방식으로 변경

--- src/app.js
+++ src/app.js
@@ -1,22 +1,7 @@
 /* @jsx createElement */
 import { createDOM, createElement, render } from "./react";
 
-// createElement 함수로 만드는 VirtualDOM
-const virtualDOM = createElement(
-  "p",
-  {},
-  createElement("h1", {}, "React Implementation"),
-  createElement(
-    "ul",
-    {},
-    createElement("li", { style: "color: red" }, "아이템1"),
-    createElement("li", { style: "color: blue" }, "아이템2"),
-    createElement("li", { style: "color: green" }, "아이템3")
-  )
-);
-render(virtualDOM, document.querySelector("#root"));
-
-const virtualDOM2 = (
+const virtualDOM = (
   <p>
     <h1>React Implementation</h1>
     <ul>
@@ -27,6 +12,4 @@ const virtualDOM2 = (
   </p>
 );
 
-console.log(virtualDOM2);
-
-render(virtualDOM2, document.querySelector("#root"));
+render(virtualDOM, document.querySelector("#root"));
