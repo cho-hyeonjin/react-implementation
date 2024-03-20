edecb8fd6e50d9f0ed01631adadd6ff16de5e295
cho-hyeonjin <gusls2005@gmail.com>
2024-03-20 22:22:55 +0900

    feat: react의 createDOM 함수 이용한 코드로 변경

--- src/app.js
+++ src/app.js
@@ -1,42 +1,6 @@
 import { createDOM, render } from "./react";
 import { createElement } from "./react";
 
-// 하드 코딩으로 만드는 VirtualDOM
-const virtualDOM = {
-  tag: "p",
-  props: {},
-  children: [
-    {
-      tag: "h1",
-      props: {},
-      children: ["React Implementation🧞‍♂️"],
-    },
-    {
-      tag: "ul",
-      props: {},
-      children: [
-        {
-          tag: "li",
-          props: { style: "color: salmon" },
-          children: ["아이템1"],
-        },
-        {
-          tag: "li",
-          props: { style: "color: skyblue" },
-          children: ["아이템2"],
-        },
-        {
-          tag: "li",
-          props: { style: "color: tan" },
-          children: ["아이템3"],
-        },
-      ],
-    },
-  ],
-};
-
-render(virtualDOM, document.querySelector("#root"));
-
 // createElement 함수로 만드는 VirtualDOM
 const virtualDOM2 = createElement(
   "p",
@@ -51,5 +15,4 @@ const virtualDOM2 = createElement(
   )
 );
 
-console.log(virtualDOM2);
 render(virtualDOM2, document.querySelector("#root"));
